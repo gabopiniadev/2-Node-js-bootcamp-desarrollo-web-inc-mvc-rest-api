015aadac139c42bd4486954b31b228aec7ddb15b
Gabriel Alejandro Piña <gabrielpinia@hotmail.com>
2023-02-02 09:52:10 -0400

    Update index.js

--- 4-proyect-rest-api/index.js
+++ 4-proyect-rest-api/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = require('./routes/index');
 const mongoose = require('mongoose');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 //Conectar MongoDB
 mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", false);
 mongoose.connect('mongodb://localhost:27017/restapis', {
-    useNewUrlParser: true 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 //Crear el Servidor
 const app = express();
 
 //Habilitar BodyParser
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Rutas de la APP
 app.use('/', router());
