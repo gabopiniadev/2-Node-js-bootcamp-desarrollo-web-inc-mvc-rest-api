d58b9c2b8fc56c387a3f5c5216218f241f3fb867
Gabriel Alejandro Piña <gabrielpinia@hotmail.com>
2023-02-02 15:05:41 -0400

    update api rest client

--- 4-proyect-rest-api/controllers/clienteController.js
+++ 4-proyect-rest-api/controllers/clienteController.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const Clientes = require('../models/Clientes');
 
 //Funcion agregar nuevo cliente
@@ -11,6 +12,65 @@ exports.nuevoCliente = async (req, res, next) => {
     }catch(error) {
         console.log(error);
         console.log('Tenemos Errores aqui!');
+        next();
+    }
+}
+
+//Funcion para mostrar clientes.
+exports.mostrarClientes = async (req, res, next) => {
+
+    try {
+        const clientes = await Clientes.find({});
+        res.json(clientes);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+//funcion para mostrar un cliente en especifico.
+exports.mostrarClienteByID = async (req, res, next) => {
+
+    try {
+        const clienteByID = await Clientes.findById(req.params.idCliente);
+
+        if(!clienteByID) {
+            res.json({mensaje: 'El cliente no existe en la data'});
+            next();
+        }else{
+            res.json(clienteByID);
+        }
+
+    } catch (error) {
+        console.log(error);
+        next();    
+    }
+}    
+
+//Funcion para crear nuevos clientes!
+exports.modificaCliente = async (req, res, next) => { 
+    
+    try {
+        const clienteByID = await Clientes.findOneAndUpdate({ _id : req.params.idCliente},
+        req.body, {
+            new : true
+        });
+        res.json(clienteByID);
+    } catch (error) {
+        console.log(error);
+        next();
+
+    }
+}
+
+//Funcion para eliminar un registro
+exports.deleteCliente = async (req, res, next) => {
+    
+    try {
+        const clienteByID = await Clientes.findOneAndDelete({ _id : req.params.idCliente});
+        res.json({mensaje: 'Cliente ha sido removido con exito!'});    
+    } catch (error) {
+        console.log(error);
         next();
     }
 }
--- 4-proyect-rest-api/routes/index.js
+++ 4-proyect-rest-api/routes/index.js
@@ -7,6 +7,17 @@ module.exports = function () {
     //Agregar Nuevos clientes
     router.post('/clientes', clienteController.nuevoCliente);
 
+    //Obtener todos los clientes
+    router.get('/clientes', clienteController.mostrarClientes);
+
+    //Obtener el cliente mediante el ID 
+    router.get('/cliente/:idCliente', clienteController.mostrarClienteByID);
+    
+    //Modificar un cliente mediante el ID.
+    router.put('/cliente/:idCliente', clienteController.modificaCliente);
+
+    router.delete('/cliente/:idCliente', clienteController.deleteCliente);
+
     
     return router;
 }
