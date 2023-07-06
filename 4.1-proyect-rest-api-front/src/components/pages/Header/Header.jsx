bda721796da4007fa291385fcf36d1760a0f2c37
Gabriel Alejandro Piña <gabrielpinia@hotmail.com>
2023-07-06 18:35:48 -0400

    update

---
+++ 4.1-proyect-rest-api-front/src/components/pages/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header class="barra">
+            <div class="contenedor">
+                <h1>CRM - Administrador de Clientes</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
